46b6e63037582dfa7fef424584a271ff6db97017
abaibik <abaibik@gmail.com>
2021-09-11 18:17:04 +0200

    class timer

--- js/timer.js
+++ js/timer.js
@@ -1,8 +1,27 @@
 loadScript("js/common.js", () => {
-  loadScript(
-    "<https://cdn.jsdelivr.net/npm/luxon@1.25.0/build/global/luxon.min.js>",
-    () => {
-      console.log("timer.js ");
-    }
-  );
+  console.log("timer.js ");
 });
+
+class Timer {
+  constructor(elemToRender) {
+    this.elemToRender = elemToRender;
+  }
+
+  start(timeout) {
+    this.secondsLeft = timeout;
+    this.timer = setInterval(this.intervalHandler, 1000);
+  }
+  stop() {
+    clearInterval(this.timer);
+  }
+  intervalHandler() {
+    this.secondsLeft -= 1;
+    this.renderSecondsLeft();
+    if (this.secondsLeft === 0) {
+      this.stop();
+    }
+  }
+  renderSecondsLeft() {
+    this.elemToRender.innerHTML = this.secondsLeft;
+  }
+}
