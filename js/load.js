f2124ec7949951e540b312b51dfe468d908a774d
abaibik <abaibik@gmail.com>
2021-09-13 18:07:16 +0200

    cache loadscript

--- js/load.js
+++ js/load.js
@@ -1,8 +1,16 @@
+const loadedUrls = [];
+
 function loadScript(url, callback) {
+  if (loadedUrls.includes(url)) {
+    callback();
+    return;
+  }
   const element = document.createElement("script");
   element.type = "text/javascript";
   element.src = url;
   element.onload = callback;
 
   document.body.appendChild(element);
+
+  loadedUrls.push(url);
 }
