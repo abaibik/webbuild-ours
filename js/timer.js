3104bbb84b67a7ceebce4fb30a4ddf32fe147ac3
abaibik <abaibik@gmail.com>
2021-09-13 11:41:52 +0200

    stop functionality

--- js/timer.js
+++ js/timer.js
@@ -3,16 +3,24 @@ loadScript("js/common.js", () => {
 });
 
 class Timer {
-  constructor(elemToRender) {
-    this.elemToRender = elemToRender;
+  constructor(renderCallback) {
+    this.renderCallback = renderCallback;
+    this.intervalHandler = this.intervalHandler.bind(this);
+    this.stop = this.stop.bind(this);
   }
 
   start(timeout) {
+    if (this.timer) {
+      return;
+    }
+    this.timeout = timeout;
     this.secondsLeft = timeout;
     this.timer = setInterval(this.intervalHandler, 1000);
+    this.renderSecondsLeft();
   }
   stop() {
     clearInterval(this.timer);
+    this.timer = undefined;
   }
   intervalHandler() {
     this.secondsLeft -= 1;
@@ -22,6 +30,31 @@ class Timer {
     }
   }
   renderSecondsLeft() {
-    this.elemToRender.innerHTML = this.secondsLeft;
+    this.renderCallback(this.secondsLeft, this.timeout);
   }
 }
+
+const buttonStart = document.getElementById("btn-start");
+const buttonStop = document.getElementById("btn-stop");
+const progressTimer = document.getElementById("progress-timer");
+const inputTimer = document.getElementById("input-timer");
+
+const timer = new Timer((secondsLeft, total) => {
+  progressTimer.innerHTML = secondsLeft;
+  const percent = (secondsLeft / total) * 100;
+  progressTimer.style = `width: ${percent}%`;
+});
+
+function run() {
+  const timeout = parseInt(inputTimer.value);
+  timer.start(timeout);
+}
+
+buttonStart.addEventListener("click", run);
+inputTimer.addEventListener("keydown", (e) => {
+  if (["Enter", "NumpadEnter"].includes(e.code)) {
+    run();
+  }
+});
+
+buttonStop.addEventListener("click", timer.stop);
