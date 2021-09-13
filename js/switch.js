ab9a9555593056b3374b254b8d51eb79392f42b4
abaibik <abaibik@gmail.com>
2021-09-13 18:07:16 +0200

    cache loadscript

--- js/switch.js
+++ js/switch.js
@@ -1,12 +1,19 @@
 export default function swithTabs(selector) {
+  let indexActive = "";
+  let timerActive = "";
+  if (window.location.href.endsWith("timer.html")) {
+    timerActive = "active";
+  } else {
+    indexActive = "active";
+  }
   const elem = document.querySelector(selector);
   elem.innerHTML = `
   <ul class="nav nav-tabs">
       <li class="nav-item">
-        <a class="nav-link active" aria-current="page" href="/index.html">Date calc</a>
+        <a class="nav-link ${indexActive}" aria-current="page" href="/index.html">Date calc</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="/timer.html">Timer</a>
+        <a class="nav-link ${timerActive}" href="/timer.html">Timer</a>
       </li>
     </ul>`;
 }
